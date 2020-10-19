3cd8247ff6eea1abd7b3c7a6e44159f11e21c671
Parag Adhau <adhauparag64@gmail.com>
2020-10-19 15:02:00 +0530

    Added :Code for forget password api

--- CommonLayer/Model/ForgetPassword.cs
+++ CommonLayer/Model/ForgetPassword.cs
@@ -6,7 +6,7 @@ namespace CommonLayer.Model
 {
     public class ForgetPassword
     {
-        //public string Email { get; set; }
-        //public string Id { get; set; }
+        public string Email { get; set; }
+        public string Id { get; set; }
     }
 }
